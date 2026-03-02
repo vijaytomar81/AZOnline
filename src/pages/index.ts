aa8593c42be161a4c6040394f1ca22abe7e1fd5b
vijaytomar81 <vijay.tomar81@gmail.com>
2026-03-02 11:35:51 +0100

    removed page imports

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,18 +2,4 @@
 export { PageManager } from "./pageManager";
 
 // Export individual pages too (optional, but useful sometimes)
-export { AuthEntryPage } from "./common/AuthEntryPage";
-
-export { InsuranceTypeSelectionPage } from "./motor/insurance-type-selection/InsuranceTypeSelectionPage";
 export { CarDetailsPage } from "./motor/car-details/CarDetailsPage";
-export { DrivingLicencePage } from "./motor/driving-licence/DrivingLicencePage";
-export { PersonalDetailsPage } from "./motor/personal-details/PersonalDetailsPage";
-export { ClaimsAndConvictionsPage } from "./motor/claims-and-convictions/ClaimsAndConvictionsPage";
-export { NoClaimDiscountPage } from "./motor/no-claim-discount/NoClaimDiscountPage";
-export { AddAdditionalDriverPage } from "./motor/add-additional-driver/AddAdditionalDriverPage";
-export { AdditionalDriverPage } from "./motor/additional-driver/AdditionalDriverPage";
-export { AdditionalDriverPersonalDetailsPage } from "./motor/additional-driver-personal-details/AdditionalDriverPersonalDetailsPage";
-export { AdditionalDriverClaimsPage } from "./motor/additional-driver-claims/AdditionalDriverClaimsPage";
-export { CarUsagePage } from "./motor/car-usage/CarUsagePage";
-export { PolicyStartDatePage } from "./motor/policy-start-date/PolicyStartDatePage";
-export { DeclinePage } from "./motor/decline/DeclinePage";
--- src/pages/pageManager.ts
+++ src/pages/pageManager.ts
@@ -1,23 +1,9 @@
 // src/pages/pageManager.ts
 import type { Page } from "@playwright/test";
 
-// Common
-import { AuthEntryPage } from "./common/AuthEntryPage";
 
-// Motor pages
-import { InsuranceTypeSelectionPage } from "./motor/insurance-type-selection/InsuranceTypeSelectionPage";
 import { CarDetailsPage } from "./motor/car-details/CarDetailsPage";
-import { DrivingLicencePage } from "./motor/driving-licence/DrivingLicencePage";
-import { PersonalDetailsPage } from "./motor/personal-details/PersonalDetailsPage";
-import { ClaimsAndConvictionsPage } from "./motor/claims-and-convictions/ClaimsAndConvictionsPage";
-import { NoClaimDiscountPage } from "./motor/no-claim-discount/NoClaimDiscountPage";
-import { AddAdditionalDriverPage } from "./motor/add-additional-driver/AddAdditionalDriverPage";
-import { AdditionalDriverPage } from "./motor/additional-driver/AdditionalDriverPage";
-import { AdditionalDriverPersonalDetailsPage } from "./motor/additional-driver-personal-details/AdditionalDriverPersonalDetailsPage";
-import { AdditionalDriverClaimsPage } from "./motor/additional-driver-claims/AdditionalDriverClaimsPage";
-import { CarUsagePage } from "./motor/car-usage/CarUsagePage";
-import { PolicyStartDatePage } from "./motor/policy-start-date/PolicyStartDatePage";
-import { DeclinePage } from "./motor/decline/DeclinePage";
+
 
 /**
  * Enterprise PageManager / Factory
@@ -43,53 +29,12 @@ export class PageManager {
         return this.cache.get(key) as T;
     }
 
-    // ----- Common -----
-    get common() {
-        return {
-            authEntry: this.get("common.authEntry", () => new AuthEntryPage(this.page)),
-        };
-    }
-
     // ----- Motor -----
     get motor() {
         return {
-            insuranceTypeSelection: this.get(
-                "motor.insuranceTypeSelection",
-                () => new InsuranceTypeSelectionPage(this.page)
-            ),
+
             carDetails: this.get("motor.carDetails", () => new CarDetailsPage(this.page)),
-            drivingLicence: this.get("motor.drivingLicence", () => new DrivingLicencePage(this.page)),
-            personalDetails: this.get("motor.personalDetails", () => new PersonalDetailsPage(this.page)),
-            claimsAndConvictions: this.get(
-                "motor.claimsAndConvictions",
-                () => new ClaimsAndConvictionsPage(this.page)
-            ),
-            noClaimDiscount: this.get(
-                "motor.noClaimDiscount",
-                () => new NoClaimDiscountPage(this.page)
-            ),
-            addAdditionalDriver: this.get(
-                "motor.addAdditionalDriver",
-                () => new AddAdditionalDriverPage(this.page)
-            ),
-            additionalDriver: this.get(
-                "motor.additionalDriver",
-                () => new AdditionalDriverPage(this.page)
-            ),
-            additionalDriverPersonalDetails: this.get(
-                "motor.additionalDriverPersonalDetails",
-                () => new AdditionalDriverPersonalDetailsPage(this.page)
-            ),
-            additionalDriverClaims: this.get(
-                "motor.additionalDriverClaims",
-                () => new AdditionalDriverClaimsPage(this.page)
-            ),
-            carUsage: this.get("motor.carUsage", () => new CarUsagePage(this.page)),
-            policyStartDate: this.get(
-                "motor.policyStartDate",
-                () => new PolicyStartDatePage(this.page)
-            ),
-            decline: this.get("motor.decline", () => new DeclinePage(this.page)),
+
         };
     }
 }
