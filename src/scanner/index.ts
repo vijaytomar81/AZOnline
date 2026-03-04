9cea91dc44a91480ec9dea696f735ac738821e75
vijaytomar81 <vijay.tomar81@gmail.com>
2026-03-04 14:40:06 +0100

    delete old folders

--- src/scanner/index.ts
+++
@@ -1,7 +0,0 @@
-// src/scanner/index.ts
-
-export { scanPage } from "./page-scanner/runner";
-export type { ScanPageOptions } from "./page-scanner/types";
-
-export type { PageMap, ScannedElement, SelectorCandidate } from "./types";
-export { createLogger } from "./logger";
