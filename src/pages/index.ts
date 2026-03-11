9135e70f2a1f025b052d855dc53de6cb8690bfeb
vijaytomar81 <vijay.tomar81@gmail.com>
2026-03-11 15:46:03 +0100

    delete files

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,19 +2,3 @@
 export { PageManager } from "./pageManager";
 
 // Export individual pages too (optional, but useful sometimes)
-
-
-
-export * from "./common/insurance-type-selection/InsuranceTypeSelectionPage";
-export * from "./common/login-or-registration/LoginOrRegistrationPage";
-export * from "./motor/ad-claims-and-convictions/AdClaimsAndConvictionsPage";
-export * from "./motor/ad-driving-licence-details/AdDrivingLicenceDetailsPage";
-export * from "./motor/ad-personal-details/AdPersonalDetailsPage";
-export * from "./motor/car-details/CarDetailsPage";
-export * from "./motor/car-usage/CarUsagePage";
-export * from "./motor/cover-start-date/CoverStartDatePage";
-export * from "./motor/ph-add-driver/PhAddDriverPage";
-export * from "./motor/ph-claims-and-convictions/PhClaimsAndConvictionsPage";
-export * from "./motor/ph-driving-licence-details/PhDrivingLicenceDetailsPage";
-export * from "./motor/ph-no-claim-discount/PhNoClaimDiscountPage";
-export * from "./motor/ph-personal-details/PhPersonalDetailsPage";
