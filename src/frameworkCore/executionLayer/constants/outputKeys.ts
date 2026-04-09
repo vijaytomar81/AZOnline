6ef9fc9981c0217e37be61ac8ae1dfe88b1dc652
vijaytomar81 <vijay.tomar81@gmail.com>
2026-04-09 17:33:02 +0200

    Update outputKeys.ts

--- src/frameworkCore/executionLayer/constants/outputKeys.ts
+++ src/frameworkCore/executionLayer/constants/outputKeys.ts
@@ -2,15 +2,12 @@
 
 export const OUTPUT_KEYS = {
     NEW_BUSINESS: {
-        START_FROM: "newBusiness.startFrom",
-        JOURNEY: "newBusiness.journey",
         CALCULATED_EMAIL: "newBusiness.calculatedEmailId",
         QUOTE: "newBusiness.quoteNumber",
         POLICY: "newBusiness.policyNumber",
         PREMIUM: "newBusiness.premium",
         OPENED_URL: "newBusiness.openedUrl",
         LAST_ACTION: "lastAction",
-        LAST_JOURNEY: "lastJourney",
         PCW_TOOL: {
             IQL: "newBusiness.pcwTool.iql",
             PAYMENT_MODE: "newBusiness.pcwTool.paymentMode",
