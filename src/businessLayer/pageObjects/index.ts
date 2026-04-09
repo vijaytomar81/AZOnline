5656b02b23828a609f2de5d3592caece6a59211f
vijaytomar81 <vijay.tomar81@gmail.com>
2026-04-09 09:42:20 +0200

    page object repair

--- src/businessLayer/pageObjects/index.ts
+++ src/businessLayer/pageObjects/index.ts
@@ -1,7 +1,10 @@
 // src/businessLayer/pageObjects/index.ts
 // AUTO-GENERATED from src/pageObjects/.manifest/
 
-export * from "./pageManager";
+export { PageManager } from "./pageManager";
+
+// Export individual pages too (optional, but useful sometimes)
+
 export * from "@businessLayer/pageObjects/objects/athena/common/insurance-type-selection/InsuranceTypeSelectionPage";
 export * from "@businessLayer/pageObjects/objects/athena/common/login-or-registration/LoginOrRegistrationPage";
 export * from "@businessLayer/pageObjects/objects/athena/common/manage-cookies/ManageCookiesPage";
