938867b35276a11cdc5613f6b5620de3cb1cb3ba
vijaytomar81 <vijay.tomar81@gmail.com>
2026-04-23 16:16:49 +0200

    Create index.ts

---
+++ src/businessLayer/businessJourneys/newBusiness/index.ts
@@ -0,0 +1,7 @@
+// src/businessLayer/businessJourneys/newBusiness/index.ts
+
+export async function runNewBusiness(): Promise<void> {
+    throw new Error(
+        "runNewBusiness is not generated yet. Please wire the new businessJourneys structure."
+    );
+}
